098823e24e7b1a406cf9a76b6aad37390073684a
Louai-AZ <azouni.louai@gmail.com>
2024-03-23 20:58:10 +0000

    Update dependencies

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
+from langchain_community.vectorstores import Pinecone
 import pinecone
-from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
-from langchain.chains import RetrievalQA
+from langchain_community.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain_community.chains import RetrievalQA
 from dotenv import load_dotenv
 from src.prompt import *
 import os
--- store_index.py
+++ store_index.py
@@ -1,19 +1,24 @@
 from src.helper import load_pdf, text_split, download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
-import pinecone
 from dotenv import load_dotenv
 import os
+
+# from langchain_community.vectorstores import Pinecone as PineconeStore
+# from langchain_community.vectorstores import Pinecone
+    # from langchain.vectorstores import Pinecone
+    # import pinecone
+# from pinecone import Pinecone, ServerlessSpec  # AttributeError: init is no longer a top-level attribute of the pinecone package.
 
 
 load_dotenv()
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
-extracted_data = load_pdf("data/")
+index_name = os.environ.get('index_name')
+
+extracted_data = load_pdf("./data/")
 text_chunks = text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 
 
-pinecone.init(api_key=PINECONE_API_KEY,
-            environment=PINECONE_API_ENV)
-index_name = os.environ.get('index_name')
-docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
+# pinecone.init(api_key=PINECONE_API_KEY,
+#             environment=PINECONE_API_ENV)
+# docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
